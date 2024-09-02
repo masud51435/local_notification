9be1fcb6b622a016366ee449fd61be67c001ad09
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-09-03 00:00:33 +0600

    complete simple notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:local_notification/pages/home_page.dart';
 
-void main() {
+void main() async {
+  await AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelGroupKey: 'basic_channel_group',
+        channelKey: 'basic_channel',
+        channelName: 'Basic Notification',
+        channelDescription: 'Notification channel description',
+        defaultColor: Colors.blue,
+        ledColor: Colors.blue,
+      ),
+    ],
+    channelGroups: [
+      NotificationChannelGroup(
+        channelGroupKey: 'basic_channel_group',
+        channelGroupName: 'Basic_group',
+      ),
+    ],
+    debug: true,
+  );
+  bool isAllowedToSendNotifications =
+      await AwesomeNotifications().isNotificationAllowed();
+  if (!isAllowedToSendNotifications) {
+    AwesomeNotifications().requestPermissionToSendNotifications();
+  }
   runApp(const MyApp());
 }
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,28 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
+import 'package:local_notification/pages/notification_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    AwesomeNotifications().setListeners(
+      onActionReceivedMethod: NotificationController.onActionReceivedMethod,
+      onNotificationCreatedMethod:
+          NotificationController.onNotificationCreatedMethod,
+      onNotificationDisplayedMethod:
+          NotificationController.onNotificationDisplayedMethod,
+      onDismissActionReceivedMethod:
+          NotificationController.onDismissActionReceivedMethod,
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +30,23 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Local Notifications'),
       ),
-      body: const Text('Local Notifications'),
+      body: const Center(
+        child: Text('Local Notifications'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          AwesomeNotifications().createNotification(
+            content: NotificationContent(
+              id: 10,
+              color: Colors.blue,
+              channelKey: 'basic_channel',
+              title: 'New Notification',
+              body: 'This is a sample notification.',
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
