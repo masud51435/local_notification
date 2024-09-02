6c0bca8b2b3f9bc3772ceebef69de6a063c4e653
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-09-02 22:59:08 +0600

    set up awesome notification

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'FLutter Local Notification',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-    home: const HomePage(),
+      home: const HomePage(),
     );
   }
 }
